62782fd9184e5292fec99b471376ba5b9d0c2815
Thao Nguyen <thao@Tobiass-MacBook-Pro.local>
2024-11-27 14:55:12 +0100

    first commit

---
+++ src/main/java/it/unisa/medical_docs_to_cda/MedicalDocumentCdaProcessorApplication.java
@@ -0,0 +1,13 @@
+package it.unisa.medical_docs_to_cda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalDocumentCdaProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalDocumentCdaProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unisa/medical_docs_to_cda/MedicalDocumentCdaProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unisa.medical_docs_to_cda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalDocumentCdaProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

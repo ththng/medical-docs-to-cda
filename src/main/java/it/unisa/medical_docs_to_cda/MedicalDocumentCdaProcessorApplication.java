746e82e39b383a10f2007d0f8e353e661c56ea21
Zeerosss <Dauria7322@gmail.com>
2024-12-04 19:01:57 +0100

    sync

--- src/main/java/it/unisa/medical_docs_to_cda/MedicalDocumentCdaProcessorApplication.java
+++ src/main/java/it/unisa/medical_docs_to_cda/MedicalDocumentCdaProcessorApplication.java
@@ -1,12 +1,5 @@
 package it.unisa.medical_docs_to_cda;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+ 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
